8c00ef169418128e951f6d9dcce16fa73ee24362
suleman-ahmad-webevis <suleman@webevis.com>
2022-12-22 15:41:03 +0500

    Murshad tester

--- src/utils/InitFacebookSDK.js
+++ src/utils/InitFacebookSDK.js
@@ -1,30 +1,30 @@
-const FACEBOOK_APP_ID = "708044773941299";
+const FACEBOOK_APP_ID = "559817135992734";
 
 export default function initFacebookSDK() {
-	return new Promise((resolve) => {
-		// Wait for the Facebook SDK to initialize before starting the React app.
-		window.fbAsyncInit = function () {
-			window.FB.init({
-				appId: FACEBOOK_APP_ID,
-				cookie: true,
-				xfbml: true,
-				version: "v15.0",
-			});
+  return new Promise((resolve) => {
+    // Wait for the Facebook SDK to initialize before starting the React app.
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        appId: FACEBOOK_APP_ID,
+        cookie: true,
+        xfbml: true,
+        version: "v15.0",
+      });
 
-			resolve();
-		};
+      resolve();
+    };
 
-		// Load the Facebook SDK script.
-		(function (d, s, id) {
-			var js,
-				fjs = d.getElementsByTagName(s)[0];
-			if (d.getElementById(id)) {
-				return;
-			}
-			js = d.createElement(s);
-			js.id = id;
-			js.src = "https://connect.facebook.net/en_US/sdk.js";
-			fjs.parentNode.insertBefore(js, fjs);
-		})(document, "script", "facebook-jssdk");
-	});
+    // Load the Facebook SDK script.
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  });
 }
