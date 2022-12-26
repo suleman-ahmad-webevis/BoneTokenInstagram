bf19f4c48c48718543f79b07a6d56119428844f1
Adnan Khan <muhammadadnan.webevis@gmail.com>
2022-12-26 11:19:04 +0500

    🚀 url changed in products service

--- src/redux/product/productService.js
+++ src/redux/product/productService.js
@@ -3,7 +3,9 @@ import { toast } from "react-toastify";
 
 //**Base URL not from Interceptor**
 // const BaseUrl = "http://localhost:5000";
-const BaseUrl = "https://backendbt.up.railway.app";
+// const BaseUrl = "https://backendbt.up.railway.app";
+const BaseUrl = "https://web-production-5b72.up.railway.app";
+
 
 const getTheProductsService = async (obj) => {
   let API_URL = `${BaseUrl}/product/products?search=${obj.search}&category=${obj.category}&featured=${obj.featured}&new=${obj.new}&gender=${obj.gender}&rated=${obj.rated}&coatColor=${obj.coatColor}&age=${obj.age}&breed=${obj.breed}&dogGroupFCI=${obj.dogGroupFCI}&season=${obj.season}&page=${obj.page}&limit=${obj.limit}`;
